ab6a0e22659f6fc6c5899cd34b45333aa4295bd7
Niraj Kumar Naik <think2secure@gmail.com>
2020-01-07 18:43:59 +0530

    functioned api

--- api.py
+++ api.py
@@ -1,12 +1,19 @@
+from typing import List
+
 import requests
+from collections import namedtuple
+
+Movie = namedtuple('Movie', 'imdb_code, title, director, keywords, '
+                            'duration, genres, rating, year, imdb_score')
 
 
-def find_movie_by_title(keyword):
+def find_movie_by_title(keyword: str) -> List[Movie]:
     url = f'http://movie_service.talkpython.fm/api/search/{keyword}'
     resp = requests.get(url)
     resp.raise_for_status()
 
     results = resp.json()
-
-    return results.get('hits')
-
+    movies = []
+    for r in results.get('hits'):
+        movies.append(Movie(**r))
+    return movies
